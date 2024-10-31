da0be650aade7e9d51465704b9355120255e0542
조수현 <josuhyeon@josuhyeon-ui-MacBookAir.local>
2024-11-01 01:43:52 +0900

    create htmls

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello my webpage!"
+    return render_template("index.html")
 
-if __name__ == "__main__": 
-    application.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
